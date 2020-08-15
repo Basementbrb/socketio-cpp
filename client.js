c53d448b73028642e5017f4fde190269c28d5f65
Timothy Vane-Tempest <tim@vane-tempest.net>
2020-08-15 13:22:27 +0200

    Added button, label and underlying logic to ping the cpp client and display results.

--- client.js
+++ client.js
@@ -1,3 +1,15 @@
 import io from 'socket.io-client';
 
-const socketio = io('http://localhost:9000');
+const socket = io('http://localhost:9000');
+let btnTimer;
+
+socket.on('web-pong', () => {
+    document.getElementById('btnlabel').innerHTML = `${performance.now() - btnTimer} ms`
+});
+
+function btnClick() {
+    btnTimer = performance.now();
+    socket.emit('web-ping');
+}
+
+document.getElementById('btn').addEventListener('click', btnClick);
--- index.js
+++ index.js
@@ -16,6 +16,17 @@ httpServer.listen(httpPort, () => {
 
 wsServer.on('connection', socket => {
     console.log('User connected');
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
+    socket.on('web-ping', () => {
+        console.log('ping from web');
+        socket.broadcast.emit('cpp-ping');
+    });
+    socket.on('cpp-pong', () => {
+        console.log('pong from cpp');
+        socket.broadcast.emit('web-pong');
+    });
 });
 
 console.log(`Listening to WebSocket requests on port ${wsPort}`);
